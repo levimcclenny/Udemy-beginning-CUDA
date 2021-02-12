40b4382fe6b4a24169c02fbada1b97d0bb3854ec
Levi McClenny <levimcclenny@tamu.edu>
2021-02-12 15:43:47 -0600

    add nthread-nblock vec_add

--- vec_addition-nthread-nblock.cu
+++ vec_addition-nthread-nblock.cu
@@ -1,9 +1,19 @@
 #include <stdio.h>
 
 __global__ void add(int *a, int *b, int *c) {
-    c[threadIdx.x] = a[threadIdx.x] + b[threadIdx.x];
+    int index = threadIdx.x + blockIdx.x * blockDim.x;
+    c[index] = a[index] + b[index];
 }
-# define N 512
+
+void random_ints(int* a, int N)
+{
+    int i;
+    for (i = 0; i < N; ++i)
+        a[i] = rand();
+}
+
+# define N (2048*2048)
+# define THREADS_PER_BLOCK 512
 int main(void) {
     int *a, *b, *c;
     int *d_a, *d_b, *d_c;
@@ -23,11 +33,11 @@ int main(void) {
     cudaMemcpy(d_b, b, size, cudaMemcpyHostToDevice);
 
     // launch add kernel on GPU with N threads
-    add<<<1, N>>>(d_a, d_b, d_c);
+    add<<<N/THREADS_PER_BLOCK, THREADS_PER_BLOCK>>>(d_a, d_b, d_c);
 
     // cp results to host
     cudaMemcpy(&c, d_c, size, cudaMemcpyDeviceToHost);
-    printf("%i\n", c);
+    scanf("%d\n", d_c);
 
     // Cleanup
     free(a); free(b); free(c);
